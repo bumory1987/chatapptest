3b1f17af9ce6e2d06b39cef54d75fd5cfbb15f8b
ys-99 <parksungbum@parkui-MacBookPro.local>
2022-02-05 16:58:17 +0900

    what

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Set;
 
-@CrossOrigin("*")
+@CrossOrigin(origins = "http://localhost:1234'")
 @RestController
 public class UserController {
 
